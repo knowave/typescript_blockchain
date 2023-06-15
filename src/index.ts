8342ee9340179114c5ad489576c34d4c3168dcca
knowave <kwaktaemin98@gmail.com>
2023-06-15 15:48:34 +0900

    create class Block

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
-import { init, exit } from "./my-package";
+import crypto from "crypto";
 
-init()
-exit()
+interface BlockShape {
+  hash: string; // 해쉬 값 (해당 블록에 관한 고유값)
+  prevHash: string; // 이전 해쉬값
+  height: number; // 1,2,3,4,5 같이 블록의 위치를 표시해주는 숫자
+  data: string; // 블록이 보호할 데이터
+}
+
+class Block implements BlockShape {
+  public hash: string; // prebHash, height, data를 가지고 계산해서 hash값을 만들어야 하므로 변수로 만듬.
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    crypto;
+    return toHash;
+  }
+}
